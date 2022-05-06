1f1a004a5a938fcff9c650dd14c57d7bbf466fda
Onda-dev <manuelaltieri3@gmail.com>
2022-05-06 15:01:26 +0200

    Check palindromo ok

--- palindroma/main.js
+++ palindroma/main.js
@@ -4,10 +4,24 @@ const parola = prompt("Inserisci una parola")
 // function isItPal (parola) {
     // Trovare la lunghezza della parola nel prompt
     const wordLength = parola.length
-    console.log(wordLength)
-    // Scorro le lettere fino a metà parola
+    // Estraggo la prima metà della parola
+    let firstHalf
     for ( let i = 0; i < wordLength / 2; i++ ) {
-        console.log(parola[i])
+        firstHalf = parola[i]
+        console.log(firstHalf)
+    }
+    // Estraggo la seconda metà della parola, al contrario
+    let secondHalf
+    for ( let i = wordLength; i > wordLength / 2 - 1; i-- ) {
+        secondHalf = parola[i]
+        console.log(secondHalf)
+    }
+    // Controllo se la prima e la seconda metà al contrario sono uguali
+    if ( firstHalf === secondHalf ) {
+        alert("Palindromo!")
+    } else {
+        alert("Non è un palindromo")
+    }
     // }
 
-}
+// }
