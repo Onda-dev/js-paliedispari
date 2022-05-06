ed9686af5d5b7468a9d2356e82fe8e01bd5fd403
Onda-dev <manuelaltieri3@gmail.com>
2022-05-06 18:09:54 +0200

    Nuova versione "corretta" ma non funzionante

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,32 +1,28 @@
 // Chiedere all’utente di inserire una parola
-const parola = prompt("Inserisci una parola")
+const word = prompt("Inserisci una parola")
 // Creare una funzione per capire se la parola inserita è palindroma
-function isItPal (parola) {
-    // Trovare la lunghezza della parola nel prompt
-    const wordLength = parola.length
-    // Estraggo la prima metà della parola
-    let firstHalf
-    for ( let i = 0; i < wordLength / 2; i++ ) {
-        firstHalf = parola[i]
-        console.log(firstHalf)
+function isItPal(word) {
+
+    let reverseWord = ""
+    for ( let i = word.length; i > word.length - 1; i-- ) {
+        reverseWord += word[i]
+        console.log(reverseWord)
+        console.log(word.length)
     }
-    // Estraggo la seconda metà della parola, al contrario
-    let secondHalf
-    for ( let i = wordLength; i > wordLength / 2 - 1; i-- ) {
-        secondHalf = parola[i]
-        console.log(secondHalf)
-    }
-    // Controllo se la prima e la seconda metà al contrario sono uguali
-    if ( firstHalf === secondHalf ) {
+    if ( word === reverseWord ) {
         return true
-    } else {
+    }
+    else {
         return false
     }
+    
 }
 
-if ( isItPal(parola) ) {
+if ( isItPal(word) ) {
     alert("Palindromo!!")
 }
 else {
     alert("Non è un palindromo...")
 }
+
+
